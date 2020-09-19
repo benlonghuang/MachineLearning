1a1cdf31965da872741cd9981fb73a24f0cf89b8
Benlong Huang <50917953+benlonghuang@users.noreply.github.com>
2020-09-19 14:19:29 -0400

    Add files via upload

---
+++ checkWin_Incomplete.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 26 08:45:20 2019
+
+@author: Samuel Fisher, Intern
+Johns Hopkins University Applied Physics Laboratory
+"""
+
+import random
+
+#Display who won and add to win counter
+def whoWin(x,End,Xwin,Owin): 
+    Xwin = 0
+    Owin = 0
+    if x == 1:
+        End.configure(text="Player 1 has won!", background = 'white')
+        Xwin = 1
+    elif x == 2:
+        End.configure(text="Player 2 has won!", background = 'white')
+        Owin = 1
+    else:
+        End.configure(text="Nobody Wins", background = 'white')
+    gameover = 1
+    L = [Xwin,Owin,gameover]
+    return L
+
+#Check if there is a three in a row
+#If there is a win, a display which team one and count that win
+def checkWin(place,AIturn,End,Xwin,Owin,turn, aiSkill): 
+    if place[1] == place[0] and place[0] == place[2] and place[1] != 0:
+        print ("Player",place[1]," wins")
+        return whoWin(place[1],End,Xwin,Owin)
+    if place[0] == place[3] and place[0] == place[6] and place[0] != 0:
+        print ("Player",place[0]," wins")
+        return whoWin(place[0],End,Xwin,Owin)
+    if place[0] == place[4] and place[0] == place[8] and place[0] != 0:
+        print ("Player",place[0]," wins")
+        return whoWin(place[0],End,Xwin,Owin)
+    if place[1] == place[4] and place[1] == place[7] and place[1] != 0:
+        print ("Player",place[1]," wins")
+        return whoWin(place[1],End,Xwin,Owin)
+    if place[2] == place[4] and place[2] == place[6] and place[2] != 0:
+        print ("Player",place[2]," wins")
+        return whoWin(place[2],End,Xwin,Owin)
+    if place[2] == place[5] and place[2] == place[8] and place[2] != 0:
+        print ("Player",place[2]," wins")
+        return whoWin(place[2],End,Xwin,Owin)
+    if place[3] == place[4] and place[3] == place[5] and place[3] != 0:
+        print ("Player",place[3]," wins")
+        return whoWin(place[3],End,Xwin,Owin)
+    if place[6] == place[7] and place[8] == place[6] and place[6] != 0:
+        print ("Player",place[6]," wins")
+        return whoWin(place[7],End,Xwin,Owin)
+    tie = 1
+    for i in place:
+        if i == 0:
+            tie = 0
+    if tie == 1:
+        return whoWin(3,End,Xwin,Owin)
+        
+    return [0,0,0]
+
+#Check who won without calling whoWin
+#Necessary for MiniMax
+def checkWin2(place):
+    if place[1] == place[0] and place[0] == place[2] and place[1] != 0:
+        return place[1]
+    if place[0] == place[3] and place[0] == place[6] and place[0] != 0:
+        return place[0]
+    if place[0] == place[4] and place[0] == place[8] and place[0] != 0:
+        return place[0]
+    if place[1] == place[4] and place[1] == place[7] and place[1] != 0:
+        return place[1]
+    if place[2] == place[4] and place[2] == place[6] and place[2] != 0:
+        return place[2]
+    if place[2] == place[5] and place[2] == place[8] and place[2] != 0:
+        return place[2]
+    if place[3] == place[4] and place[3] == place[5] and place[3] != 0:
+        return place[3]
+    if place[6] == place[7] and place[8] == place[6] and place[6] != 0:
+        return place[6]
+    if list(filter(lambda x: x == 0, place)) == []:
+        return 0
+    return None
+
+def checkWinPos(place):
+    # if AI can win in one step, take that move.
+    if checkWin2(place) is not None:
+        return None
+    G = place.copy()
+    for i in range(len(G)):
+        if G[i] == 0:
+            G[i] = 2
+            if checkWin2(G) == 2:
+                return i
+            G[i] = 0
+    # if player can win in next step, block that move.
+    for i in range(len(G)):
+        if G[i] == 0:
+            G[i] = 1
+            if checkWin2(G) == 1:
+                return i
+            G[i] = 0
+    # if the center is empty, take this move
+    if G[4] == 0:
+        return 4
+    # otherwise, take a random move
+    while True:
+        i = random.randint(0, 8)
+        if G[i] == 0:
+            return i
